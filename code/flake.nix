5a1eea5ab15191e86ece0aad8d39c6c5576c0687
Jeppe Allerslev <jeppeallerslev@gmail.com>
2025-02-12 11:09:13 +0100

    foo

---
+++ code/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          system = system;
+          config = { allowUnfree = true; };
+        }; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [ 
+              openssl
+              (python312.withPackages(ps: with ps; [
+                ipython
+                ipykernel
+                jupyter
+                jupyter-collaboration
+                notebook
+                tqdm
+                matplotlib
+                numpy
+                scikit-learn
+                pandas
+                seaborn
+              ]))
+            ];
+
+            shellHook = ''
+            '';
+          };
+        }
+      );
+}

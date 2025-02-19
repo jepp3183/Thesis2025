159712d58fb6b0dbd750293e53a732ba6aaafd5a
au682845 <au682845@uni.au.dk>
2025-02-19 11:56:09 +0100

    added feature penalty

--- code/lib/CF_De/cfde.py
+++ code/lib/CF_De/cfde.py
@@ -1,7 +1,34 @@
 import numpy as np
 import random
 
-def Simple_CF_Descent(df, model, target, centers, instance_index=-1, stop_count=50, step_size=0.05, limit = 1000, dis = lambda a,b : euclid_dis(a,b)):
+def Simple_CF_Descent(
+        df, 
+        target, 
+        centers, 
+        model=None, 
+        instance_index=-1, 
+        stop_count=100, 
+        step_size=0.05, 
+        limit = 10000,
+        feature_penalty = 1.001, 
+        dis = lambda a,b : euclid_dis(a,b)):
+
+    predictor = None
+    if model == None:
+        def center_prediction(p):
+            current_best_center = 0
+            current_best_distance = float("inf")
+            for i in range(len(centers)):
+                distance = dis(p,centers[i])
+                if distance < current_best_distance:
+                    current_best_center = i
+                    current_best_distance = distance
+            return float(current_best_center)
+
+        predictor = lambda p : center_prediction(p)
+    else:
+        predictor = lambda p : model.predict([p])[0]
+
     cf = centers[int(target)]
     target_points = df[df["label"] == target]
 
@@ -22,39 +49,53 @@ def Simple_CF_Descent(df, model, target, centers, instance_index=-1, stop_count=
     instance = instance[:-1]
 
     it = 0
+    changed_features = []
     while misses < stop_count and it < limit:
         y = target_points.sample().values[0][:-1]
         changes = []
         current_dis = dis(cf, instance)
 
         for i in range(len(y)):
+            penalty = 1.0
+            if i not in changed_features:
+                if len(changed_features) == 0:
+                    penalty = 1.0
+                else:
+                    penalty = np.pow([feature_penalty], [len(changed_features)])[0]
+
+            print(penalty)
+
             cf_prime = cf.copy()
 
             step = y[i] - cf_prime[i]
             
             cf_prime[i] += step * step_size
 
-            distance_new = dis(cf_prime, instance)
+            distance_new = dis(cf_prime, instance) * penalty
 
             if distance_new < current_dis:
-                changes.append((cf_prime,distance_new))
+                changes.append((cf_prime,distance_new, i))
 
         if len(changes) == 0:
             misses += 1
         else:
-            best = min(changes, key = lambda x: x[1])[0]
+            best = min(changes, key = lambda x: x[1])
+            f = best[2]
+            best = best[0]
             if all([x == y for x,y in zip(cf,best)]):
                 misses += 1
             else:    
                 try:
-                    prediction = model.predict([best])
-                    if prediction[0] != int(target):
+                    prediction = predictor(best)
+                    if prediction != int(target):
                         misses += 1
                         it += 1
                     else:
                         cf = best
                         misses = 0
                         history.append(best)
+                        if f not in changed_features:
+                            changed_features.append(f)
                 except ValueError:
                     print("fail")
                     misses += 1
@@ -63,6 +104,7 @@ def Simple_CF_Descent(df, model, target, centers, instance_index=-1, stop_count=
     if len(history) == 0:
         history.append((cf,0))
     print("Amount of changes: ", len(history))
+    print("Number of changed features:",len(changed_features))
     return instance,cf,history
 
 def euclid_dis(x,y):

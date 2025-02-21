2aa3a7a9d9311942f7429de4ab7b01e553e78062
Jeppe Allerslev <jeppeallerslev@gmail.com>
2025-02-21 23:45:43 +0100

    update flake

--- code/flake.nix
+++ code/flake.nix
@@ -7,15 +7,57 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs {
-          system = system;
-          config = { allowUnfree = true; };
-        }; in
+        let 
+          pkgs = import nixpkgs {
+            system = system;
+            config = { allowUnfree = true; };
+          }; 
+
+          pythonPackages = pkgs.python312Packages;
+
+          raiutils = 
+            pythonPackages.buildPythonPackage rec {
+              pname = "raiutils";
+              version = "0.4.2";
+              doCheck = false;
+              src = pkgs.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-0hCk1aBZ5IOI00HuAsuH88krvx8Ly87PBP2TpZnS3KQ=";
+              };
+
+              dependencies = [
+                pythonPackages.numexpr
+              ];
+            };
+
+          dice-ml = 
+            pythonPackages.buildPythonPackage rec {
+              pname = "dice_ml";
+              version = "0.11";
+              doCheck = false;
+              src = pkgs.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-Kea+qeTId8qmjs3V2YH5HMjeO3PNrUU/18fgAKM2V20=";
+              };
+              dependencies = [
+                raiutils
+              ];
+            };
+
+            mmd-critic = 
+              pythonPackages.buildPythonPackage rec {
+                pname = "mmd_critic";
+                version = "0.1.2";
+                doCheck = false;
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  sha256 = "sha256-IWWkAGGRKT8MHPrEyvIRJIsfQXJFaQEHymHJWlhfs2k=";
+                  };
+              };
+        in
         {
           devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [ 
-              openssl
-              (python312.withPackages(ps: with ps; [
+            buildInputs = with pythonPackages; [ 
                 ipython
                 ipykernel
                 jupyter
@@ -26,7 +68,8 @@
                 scikit-learn
                 pandas
                 seaborn
-              ]))
+                dice-ml
+                mmd-critic
             ];
 
             shellHook = ''
